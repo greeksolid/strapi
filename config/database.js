860748918ae67fa30506ad44414a1156dc5b39f6
Tasos Georgoulis <tasosgeo2005@hotmail.com>
2023-02-16 13:47:40 +0200

    modify env

--- config/database.js
+++ config/database.js
@@ -1,13 +1,23 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+module.exports = ({ env }) => {
+  return {
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
-      ssl: env.bool(true),
+      client: "postgres",
+      connection: {
+        host: process.env.PGHOST,
+        port: process.env.PGPORT,
+        database: process.env.PGDATABASE,
+        user: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        ssl: env.bool(true),
+      },
+      // connection: {
+      //   host: env("PGHOST", "127.0.0.1"),
+      //   port: env.int("PGPORT", 5432),
+      //   database: env("PGDATABASE", "strapi"),
+      //   user: env("PGUSER", "strapi"),
+      //   password: env("PGPASSWORD", "password"),
+      //   ssl: env.bool(true),
+      // },
     },
-  },
-});
+  };
+};
